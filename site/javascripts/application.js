81da5f11f931a94fe424da5f25af4f024096b054
Jon Suderman <jon@suderman.me>
2010-01-31 13:29:14 -0700

    Nearing completion of layout

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -7,6 +7,17 @@ document.observe("dom:loaded", function() {
   
   var a = new Anns()
   var c = new Common()
+  // new Carousel('#footer .carousel', { transition: 'fade' })
+  
+  
+  new Carousel({ 
+    container           : '#footer .carousel', 
+    delay               : 4,
+    duration            : 2,
+    transition          : 'crossfade',           // crossfade, fade, cut, push
+    slides_easing       : 'easeInOutCubic',     // bounce, bouncePast, easeFrom, easeFromTo, easeInBack, easeInCirc, easeInCubic, easeInExpo, easeInOutBack, easeInOutCirc, easeInOutCubic, easeInOutExpo, easeInOutQuad, easeInOutQuart, easeInOutQuint, easeInOutSine, easeInQuad, easeInQuart, easeInQuint, easeInSine, easeOutBack, easeOutBounce, easeOutCirc, easeOutCubic, easeOutExpo, easeOutQuad, easeOutQuart, easeOutQuint, easeOutSine, easeTo, elastic, linear, sinusoidal, spring, swingFrom, swingFromTo, swingTo
+  })  
+  
 })
 
 // Window onload
